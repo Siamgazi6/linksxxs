af2e587a5973e23a9eb8e5534b97a0370f223f51
Siamgazi6 <89618597+Siamgazi6@users.noreply.github.com>
2026-08-23 00:47:48 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,27 +4,26 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = 'https://dev-test8568.pantheonsite.io/graphql';
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
-	
-    // FIX 2: pathArr আনডিফাইন্ড বা স্ট্রিং হলে তা নিরাপদে হ্যান্ডেল করা
-	const pathArr = ctx.query.postpath;
-	const path = Array.isArray(pathArr) ? pathArr.join('/') : (pathArr || '');
-	
+	const pathArr = ctx.query.postpath as Array<string>;
+	const path = pathArr.join('/');
 	console.log(path);
 	const fbclid = ctx.query.fbclid;
 
 	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') || fbclid) {
+		if (referringURL?.includes('facebook.com') || fbclid) {
+
 		return {
 			redirect: {
 				permanent: false,
-				destination: `https://saveourstateok.org/${encodeURI(path as string)}`,
+				destination: `${
+					`https://saveourstateok.org/` + encodeURI(path as string)
+				}`,
 			},
 		};
-	}
-
+		}
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
@@ -51,18 +50,16 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	`;
 
 	const data = await graphQLClient.request(query);
-	
 	if (!data.post) {
 		return {
 			notFound: true,
 		};
 	}
-	
 	return {
 		props: {
 			path,
 			post: data.post,
-			host: ctx.req.headers.host || '',
+			host: ctx.req.headers.host,
 		},
 	};
 };
@@ -74,7 +71,7 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = (props) => {
-	const { post, host } = props;
+	const { post, host, path } = props;
 
 	// to remove tags from excerpt
 	const removeTags = (str: string) => {
@@ -93,30 +90,19 @@ const Post: React.FC<PostProps> = (props) => {
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
-				
-                {/* FIX 1: মেটা ইমেজগুলো যদি থাকে, তবেই কেবল রেন্ডার হবে */}
-				{post.featuredImage?.node?.sourceUrl && (
-					<>
-						<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-						<meta
-							property="og:image:alt"
-							content={post.featuredImage.node.altText || post.title}
-						/>
-					</>
-				)}
+				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta
+					property="og:image:alt"
+					content={post.featuredImage.node.altText || post.title}
+				/>
 				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
 				<h1>{post.title}</h1>
-				
-                {/* FIX 1: ইমেজ এলিমেন্ট যদি থাকে, তবেই কেবল রেন্ডার হবে */}
-				{post.featuredImage?.node?.sourceUrl && (
-					<img
-						src={post.featuredImage.node.sourceUrl}
-						alt={post.featuredImage.node.altText || post.title}
-					/>
-				)}
-				
+				<img
+					src={post.featuredImage.node.sourceUrl}
+					alt={post.featuredImage.node.altText || post.title}
+				/>
 				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
 		</>
